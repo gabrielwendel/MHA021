6233c872b0f659455e41e6d1a6fd57ddc67eaceb
gabrielwendel <gabriel.wendel990217@gmail.com>
2023-11-20 14:15:02 +0100

    Task 1 c)

--- convecte.m
+++ convecte.m
@@ -16,5 +16,17 @@ function [ Kce, fce ] = convecte( ex, ey, alpha, thick, Tamb )
 % Created by: Martin Fagerström 2020-11-10
 %-------------------------------------------------------------------
 
+L_ex=ex(2)-ex(1);
+L_ey=ey(2)-ey(1);
+L_e=sqrt(L_ex^2+L_ey^2);
+
+syms s
+
+N_e=[1-s/L_e, s/L_e];
+
+Kce=int(N_e'*N_e*alpha,s,0,L_e);
+fce=int(N_e'*alpha*Tamb,s,0,L_e);
+
+
 %Please complete the function file ot kalculate Ke and fce
 
